73a6c6df2f941dd3ede1c7279d6b0e2316dd34ea
Pawel <p380623@gmail.com>
2024-05-06 19:48:03 +0200

    #1

---
+++ src/main/java/project/teamDirectory/TeamDirectoryApplication.java
@@ -0,0 +1,13 @@
+package project.teamDirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/teamDirectory/TeamDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package project.teamDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
